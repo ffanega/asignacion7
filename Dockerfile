819be71c7f9e332c09e8e168e14642587cd46c21
Pedro Fanega <14-10332@usb.ve>
2021-03-28 13:51:30 -0400

    Modificacion para realizar revision periodica y escritura de archivo .json. Nuevo Dockerfile. Nuevo Gitignore

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM python
+
+RUN pip install requests
+RUN pip install simplejson
+
+ADD asignacion7.py /
+
+CMD ["python", "./asignacion7.py"]
+
+
--- asignacion7.py
+++ asignacion7.py
@@ -1,23 +1,41 @@
 import requests
+import simplejson as json
+import time
+import os
+
 from pprint import pprint
 
-response = requests.post(
-    'https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token',
-    headers={'Authorization':'Basic ZGV2bmV0dXNlcjpDaXNjbzEyMyE='})
-response.raise_for_status()
-payload=response.json()
-pprint(payload)
-headers2 = {
-        'X-Auth-Token' : payload.get('Token')
-}
-response2 = requests.get("https://sandboxdnac.cisco.com/dna/intent/api/v1/network-device",headers=headers2)
-payload2 = response2.json()
-pprint(payload2)
-payload2=payload2.get('response')
-tabla = []
-for org in payload2:
-    tabla.append(dict(family=org.get('family'), hostname=org.get('hostname'),
-                      managementIpAddress=org.get('managementIpAddress'),
-                      lastUpdated=org.get('lastUpdated'), reachabilityStatus=org.get('reachabilityStatus')))
-pprint(tabla)
+inicio = time.time()
+i=0
+
+while True:
+
+    response = requests.post('https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token', headers={'Authorization':'Basic ZGV2bmV0dXNlcjpDaXNjbzEyMyE='})
+    response.raise_for_status()
+    payload=response.json()
+    headers2 = {
+            'X-Auth-Token' : payload.get('Token')
+    }
+    response2 = requests.get("https://sandboxdnac.cisco.com/dna/intent/api/v1/network-device",headers=headers2)
+    payload2 = response2.json()
+    payload2=payload2.get('response')
+    
+    tabla = []
+
+    for org in payload2:
+        tabla.append(dict(hostname=org.get('hostname'), reachabilityStatus=org.get('reachabilityStatus')))
+
+    i = i+1
+    
+    with open ('tarea8.json','w') as f:
+        json.dump(tabla,f)
+
+    final= time.time()
+    print ("El archivo se actualizo", i, "veces")
+    time.sleep(300)
+    
+    if final - inicio >= 3000:
+        break
+    
+    print ("El programa ha finalizado")
 
